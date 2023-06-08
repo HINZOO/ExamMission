4dcfa6942e188c9d7375ca7d07e6cbd42eeb5640
HIMJU <hinge1128@gmail.com>
2023-06-08 20:45:05 +0900

    mission4Ing2

--- src/main/java/com/example/demo/dto/PageDto.java
+++ src/main/java/com/example/demo/dto/PageDto.java
@@ -1,11 +1,48 @@
 package com.example.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class PageDto {
     private int pageNum=1;
     private int pageSize=5;
-    private String order="e_id DESC";
+    
+    enum ExamsType {
+       e_id,u_id,post_time,to_time,from_time,name,nation,city
+    }
+    
+    enum DirectType{
+        DESC,ASC,
+    }
+    private ExamsType order= ExamsType.post_time;
+    private DirectType direct=DirectType.DESC;
+
+    private ExamsType searchField;
+    @JsonProperty("eId")
+    private int eId;
+    @JsonProperty("uId")
+    private String uId;
+    private String name;
+    private String nation;
+    private String city;
+    private String postTime;
+    private String gender;
+    private String toTime;
+    private String fromTime;
+}
+    
+    private String searchValue;
+    private String orderBy;
+    public String getOrderBy() {//쿼리로 넘어간 값이 없는 경우 공백으로 지정.
+        if(this.order!=null && this.direct!=null) {
+            return this.order+" "+this.direct;
+        }else if (this.order!=null){
+            this.direct=DirectType.ASC;
+            return this.order+" "+this.direct;
+        }
+        return ExamsType.post_time+" " +DirectType.DESC;//SELECT * FORM boards ORDER BY  기본값.
+    }
 
 }
